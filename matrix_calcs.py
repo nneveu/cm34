664bb54155db0dcdec49470ade63a7fef9dbac57
nneveu <nneveu@stanford.edu>
2024-06-19 23:24:45 -0700

    using dipole/corrector

--- matrix_calcs.py
+++ matrix_calcs.py
@@ -3,16 +3,29 @@ import numpy as np
 # Constants
 l1 = 2.4 # m
 l2 = 1.5 # m 
-klx = 19  # G-m
-kly = -25 # G-m
-cl = 0.01 # corrector length in meters
+kx = 0.0019  # T-m = 19  G-m
+ky = 0.0025 # T-m = -25 G-m - taking absolute value
+cl = 1 # corrector length in meters
+
+# Multiply field strength by length to get T units
+# Assuming momentum and kinetic energy differences are neglible (electrons)
+def calc_rho_theta(energy, k, l):
+    rho   = energy / (0.3*k*l)
+    theta = l/rho
+    print('rho', rho, 'theta', theta)
+    return rho, theta
+
+energy = 3.0 # GeV
+rhox, thetax = calc_rho_theta(energy, kx, cl) 
+rhoy, thetay = calc_rho_theta(energy, ky, cl)
+
 
 # Initial conditions
 xinit_offset = 0.001 #m - Say we start w/ 1 mm offset
-xinit_angle  = 0 # Say we start with no angle (best case)
+xinit_angle  = 0.1 # Say we start with a very small angle (best case)
 
 yinit_offset = 0.001 #m - Say we start w/ 1 mm offset
-yinit_angle  = 0 # Say we start with no angle (best case)
+yinit_angle  = 0.01 # Say we start with no angle (best case)
 
 x0 = np.array([[xinit_offset],
                [0]])
@@ -26,16 +39,16 @@ m_d1 = np.array([[1, l1],
 m_d2 = np.array([[1, l2],
                  [0, 1]])
 
-q_cmx = np.array([[1, 0],
-                 [klx, 1]])
+#q_cmx = np.array([[1, 0],
+#                 [klx, 1]])
+#
+#q_cmy = np.array([[1, 0],
+#                 [kly, 1]])
 
-q_cmy = np.array([[1, 0],
-                 [kly, 1]])
-
-c_cmx = np.array([[1, cl],
+c_cmx = np.array([[1, rhox*np.sin(thetax)],
                  [0, 1]])
 
-c_cmy = np.array([[1, cl],
+c_cmy = np.array([[1, rhoy*np.sin(thetay)],
                  [0, 1]])
 
 def calc_offsets(minitial, m_d1, m_d2, q_cm):
@@ -45,8 +58,8 @@ def calc_offsets(minitial, m_d1, m_d2, q_cm):
     x2_xp2    = np.matmul(xd2_q1_d1, minitial)
     return x2_xp2
 
-xoffsets = calc_offsets(x0, m_d1, m_d2, q_cmx)
-yoffsets = calc_offsets(y0, m_d1, m_d2, q_cmy) 
+xoffsets = calc_offsets(x0, m_d1, m_d2, c_cmx)
+yoffsets = calc_offsets(y0, m_d1, m_d2, c_cmy) 
 
 print('Beam offset at CM34 in x: ', xoffsets[0][0]*1000, 'mm')
 print('Beam offset at CM34 in y: ', yoffsets[0][0]*1000, 'mm')
